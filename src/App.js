c7b06d1fc01a9e7fb0af8609f2bb7bd092d65ee3
alexandra trivino <alexandratrivino2@gmail.com>
2023-09-29 20:43:27 -0500

    first exercise solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ color, setColor ] =  useState('red');
+  const newColor = color === 'red' ? 'blue' : 'red';
+  const [ enabled, setEnabled ] = useState(false);
+  const [ isChecked, setIsChecked ] = useState(false);
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+    setEnabled(!isChecked);
+  };
+  console.log(isChecked);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{backgroundColor: color}} onClick={() => setColor(newColor)} disabled={enabled}>Change to {color}</button>
+      <input type="checkbox" checked={isChecked} onChange={handleChange}/>
     </div>
   );
 }
