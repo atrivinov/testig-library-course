e06b18b72964a43b7def5563dab02511a70410d9
alexandra trivino <alexandratrivino2@gmail.com>
2023-09-29 22:37:10 -0500

    testing a function

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { logRoles, render, screen, fireEvent } from '@testing-library/react';
+import { replaceCamelWithSpaces } from './App';
 import App from './App';
 
-test('It changes the state depending on the event', () => {
+it('It changes the state depending on the event', () => {
   render(<App />);
 
   const checkbox = screen.getByRole('checkbox', { name: 'Disable Button' });
@@ -14,4 +15,16 @@ test('It changes the state depending on the event', () => {
   fireEvent.click(checkbox);
   expect(colorButton).toBeEnabled();
   expect(colorButton).toHaveStyle('background-color: red');
+});
+
+describe('spaces before camel-case capital letters', () => {
+  it('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+  it('Works for one inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+  it('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
 });
